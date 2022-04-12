9c4388f9f9b75cc79f94019a5eb1ea0ea1e7dd76
littldude <oksana.ryabchenko14@gmail.com>
2022-04-12 16:55:55 +0300

    filter.js

--- js/filter-show-hide.js
+++ js/filter-show-hide.js
@@ -1,10 +1,16 @@
 $(document).ready(function($) {
     let textFilter = document.getElementById('link--filter');
-    textFilter.textContent = 'Скрыть фильтр';
+
+    if (window.matchMedia("screen and (min-width: 1px) and (max-width:992px)").matches) {
+        textFilter.textContent = 'Фильтр';
+    }
+    else {
+        textFilter.textContent = 'Скрыть фильтр';
+    }
+    
     $('.filter__button').on("click",function(event) {
         let filterBlock = this.parentNode.parentNode.children[4].children[1];
         let filterButton = this.parentNode.children[1];
-        console.log(filterBlock)
         filterBlock.classList.toggle('is_active');
         let is_active = filterBlock.classList.contains('is_active');
         filterBlock.style.display = (!is_active ? 'block' : 'none');
@@ -17,6 +23,7 @@ $(document).ready(function($) {
         filterBlock.classList.toggle('is_active');
         let is_active = filterBlock.classList.contains('is_active');
         filterBlock.style.display = (!is_active ? 'block' : 'none');
+        textFilter.textContent = (!is_active ? 'Фильтр' : 'Фильтр');
     });
 });
 
