9b698647b270a68f132057fccaaac7eaf57851d3
littldude <oksana.ryabchenko14@gmail.com>
2022-04-12 16:17:38 +0300

    Correct js and scss

--- js/button-more-items.js
+++ js/button-more-items.js
@@ -1,7 +1,7 @@
 $('.button--content').on("click",function(event) {
     event.preventDefault(); 
-    let collectionOfItems = document.getElementById('content__inner').innerHTML;
-    let dest = document.getElementById('content__inner'); 
+    let collectionOfItems = document.getElementById('content__cards').innerHTML;
+    let dest = document.getElementById('content__cards'); 
     console.log(collectionOfItems)
     for (let i = 0; i < 1; i++) {
         dest.innerHTML += collectionOfItems;
--- js/sorting.js
+++ js/sorting.js
@@ -1,6 +1,6 @@
 
 $('.filter__inner--item__date').on("click",function() {
-    var $wrapper = $('.content__inner');
+    var $wrapper = $('.content__cards');
 
     $wrapper.find('[data-date]').sort(function(a, b) {
     return +a.dataset.date - +b.dataset.date;
@@ -8,7 +8,7 @@ $('.filter__inner--item__date').on("click",function() {
     .appendTo($wrapper);
 });    
 $('.filter__inner--item__popular').on("click",function() {
-    var $wrapper = $('.content__inner');
+    var $wrapper = $('.content__cards');
 
     $wrapper.find('[data-popular]').sort(function(a, b) {
     return +a.dataset.popular - +b.dataset.popular;
@@ -16,7 +16,7 @@ $('.filter__inner--item__popular').on("click",function() {
     .appendTo($wrapper);
 }); 
 $('.filter__inner--item__price').on("click",function() {
-    var $wrapper = $('.content__inner');
+    var $wrapper = $('.content__cards');
 
     $wrapper.find('[data-price]').sort(function(a, b) {
     return +a.dataset.price - +b.dataset.price;
